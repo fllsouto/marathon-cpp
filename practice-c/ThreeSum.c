a840cf27897508f73775c91275f5adcfcab90446
Fellipe Souto <fllsouto@gmail.com>
2014-08-21 11:23:09 -0300

    Coding three sum brutal force alg.

--- practice-c/ThreeSum.c
+++ practice-c/ThreeSum.c
@@ -3,6 +3,8 @@
 
 void print_sequence(int *,int);
 void read_sequence(int *,int);
+int three_sum_brutal_force(int *,int);
+void show_triple(int, int, int);
 
 int main (){
 	
@@ -10,6 +12,7 @@ int main (){
 	int num;
 	int x;
 	int i;
+	int finalQnt = 0;
 
 	printf("Quanto numeros gostaria de inserir?\n");
 	scanf("%d",&num);
@@ -17,15 +20,37 @@ int main (){
 	vec = malloc(num*sizeof(int));
 	
 	read_sequence(vec,num);
-	print_sequence(vec,num);
+	finalQnt = three_sum_brutal_force(vec,num);
+	printf("Quantidade encontrada : %d\n", finalQnt);
 	free (vec);
 	return 0;
+}
+int three_sum_brutal_force(int *vec,int num){
+	int i;
+	int j;
+	int k;
+	int qnt = 0;
+
+	for (i = 0; i < num; i++){
+		for (j = i+1; j < num; j++){
+			for (k = j+1; k < num; k++){
+				if (vec[i] + vec[j] + vec[k] == 0){
+					qnt++;
+					// show_triple(vec[i],vec[j],vec[k]);
+				}
+			}
+		}
+	}
+	return qnt;
+}
+
+void show_triple(int i, int j, int k){
+	printf("(%d,%d,%d)\n", i,j,k);
 }
 
 void read_sequence(int *vec,int num){
 	int i;
 	int x;
-	printf("Estou aqui dentro!\n");
 	for(i = 0; i < num; i++){
 		printf("Insira o #%d : ", i);
 		scanf("%d",&x);
