2ef78a84efaa281584a7f87ca08d0b65d2123995
Fellipe Souto <fllsouto@gmail.com>
2014-08-21 10:49:30 -0300

    Practicing some basic coding

--- practice-c/ThreeSum.c
+++ practice-c/ThreeSum.c
@@ -1,6 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void print_sequence(int *,int);
+void read_sequence(int *,int);
 
 int main (){
-	printf("Hello Todos");
+	
+	int *vec;
+	int num;
+	int x;
+	int i;
+
+	printf("Quanto numeros gostaria de inserir?\n");
+	scanf("%d",&num);
+	printf("Quantidade de numeros inseridos : %d\n", num);
+	vec = malloc(num*sizeof(int));
+	
+	read_sequence(vec,num);
+	print_sequence(vec,num);
+	free (vec);
 	return 0;
+}
+
+void read_sequence(int *vec,int num){
+	int i;
+	int x;
+	printf("Estou aqui dentro!\n");
+	for(i = 0; i < num; i++){
+		printf("Insira o #%d : ", i);
+		scanf("%d",&x);
+		vec[i] = x;	
+	}
+}
+
+void print_sequence(int *vec,int num){
+	int i;
+	printf("Estou aqui dentro!\n");
+	for(i = 0; i < num; i++){
+		printf("#%d : %d\n", i,vec[i]);
+	}
 }
