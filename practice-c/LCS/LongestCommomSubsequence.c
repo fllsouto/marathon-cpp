fc69f52547d8d12471efcd20d14c1e41133e8fa6
Fellipe Souto <fllsouto@gmail.com>
2015-05-13 22:48:26 -0300

    stable version of right cls calculation with L vector

--- practice-c/LCS/LongestCommomSubsequence.c
+++ practice-c/LCS/LongestCommomSubsequence.c
@@ -4,59 +4,34 @@
 
 #define MAX(A,B) (((A) > (B))? (A) : (B))
 
-int **left_c;
 
-void left_memoized_LCS(int , int);
-void right_memoized_LCS(int**, int , int);
-int lookup_LCS(int** ,char*, int, char*, int);
+void free_matrix(int** , int);
 void show_matrix(int**, int, int);
-void free_matrix(int** , int , int );
+void calculate_L_vector(int**, char*, int, char*, int, int*);
+void calculate_right_LCS_matrix(int** ,char*, int, char*, int);
+void initialize_right_LCS_matrix(int**, int , int);
 void calculate_right_LCS(char *,char *);
+char* get_string();
 
 int main(){
   
-  char word1[] = "ABCBDAB";
-  char word2[] = "BDCABA";
-  int x_l = (unsigned)strlen(word1);
-  int i;
-  for(i = 0; i < x_l; i++){
-    calculate_right_LCS(word1 + i, word2);
-  }
-  // calculate_right_LCS("B", word2);
+  char *word1 = NULL;
+  char *word2 = NULL;
+
+  word1 = get_string();
+  word2 = get_string();
+
+  calculate_right_LCS(word1, word2);
   printf("\n");
   return 0;
 }
 
-
-
-int left_lookup_LCS(char* X, int m, char* Y, int n){
-  int i,j;
-  printf("\n");
-  for(i = 1; i < m; i++){
-    for(j = 1; j < n; j++){
-      if(X[i-1] == Y[j-1]){
-        left_c[i][j] = left_c[i-1][j-1] + 1;
-      }else{
-        left_c[i][j] = MAX(left_c[i-1][j], left_c[i][j-1]);
-      }
-    }
+void free_matrix(int** v, int m){
+  int i;
+  for(i = 0; i < m; i++){
+      free(v[i]);
   }
-  return left_c[i-1][j-1];
-}
-
-int right_lookup_LCS(int** right_c, char* X, int x_size, char* Y, int y_size){
-  int i,j;
-  for(i = x_size - 1; i >= 0; i--){
-    for(j = y_size - 1; j >= 0; j--){
-      if(X[i] == Y[j]){
-        printf("(%d,%d) %c :: %c\n", i,j,X[i],Y[j]);
-        right_c[i][j] = right_c[i+1][j+1] + 1;
-      }else{
-        right_c[i][j] = MAX(right_c[i+1][j], right_c[i][j+1]);
-      }
-    }
-  }
-  return right_c[i+1][j+1];
+  free(v);
 }
 
 void show_matrix(int** v,int m,int n){
@@ -68,60 +43,80 @@ void show_matrix(int** v,int m,int n){
     }
     printf ("\n");
   }
-    printf ("--------------------");
+    printf ("-----------------------------------------------------------------");
     printf ("\n");
 }
 
-void left_memoized_LCS(int m, int n){
-  int i,j;
-  for(i = 0; i < m; i++){ left_c[i][0] = 0; }
-  for(j = 0; j < n; j++){ left_c[0][j] = 0; }
+void show_L_vector(int* l,char* X, int x_size, char* Y){
+  int i;
+  for(i = 0; i < x_size; i++){ printf("%s --- %s\nL[%d] : %d\n", X + i, Y, i, l[i]); }
 }
-void right_memoized_LCS(int** right_c, int m, int n){
+
+
+void calculate_L_vector(int**right_c, char* X, int x_size, char* Y, int y_size, int* vectorL){
+  int i;
+  for(i = 0; i < x_size; i++){ vectorL[i] = right_c[i][0]; }
+}
+
+void calculate_right_LCS_matrix(int** right_c, char* X, int x_size, char* Y, int y_size){
+  int i,j;
+  for(i = x_size - 1; i >= 0; i--){
+    for(j = y_size - 1; j >= 0; j--){
+      if(X[i] == Y[j]){
+        right_c[i][j] = right_c[i+1][j+1] + 1;
+      }else{
+        right_c[i][j] = MAX(right_c[i+1][j], right_c[i][j+1]);
+      }
+    }
+  }
+}
+
+void initialize_right_LCS_matrix(int** right_c, int m, int n){
   int i,j;
   for(i = 0; i < m; i++){ right_c[i][n-1] = 0; }
   for(j = 0; j < n; j++){ right_c[m-1][j] = 0; }
 }
 
-void free_matrix(int** v, int m, int n){
-  int i;
-  for(i = 0; i < m; i++){
-      free(v[i]);
-  }
-  free(v);
-}
 void calculate_right_LCS(char* wordX,char* wordY){
   int x_size = (unsigned)strlen(wordX);
   int y_size = (unsigned)strlen(wordY);
-  printf("Ysize : %d\nXsize : %d\n", y_size,x_size);
   int m = x_size + 1;
   int n = y_size + 1;
   int i,j;
   int llcs ;
   int rlcs ;
   int **right_c;
+  int *vectorL;
 
-
-  // printf("Palavra X : %s \nPalavra Y : %s\n", wordX,wordY);
-  // left_c = malloc(m*sizeof(int *));
+  vectorL = malloc(x_size*sizeof(int));
   right_c = malloc(m*sizeof(int *));
 
   for(i = 0; i < m; i++){
-    left_c[i] = malloc(n*sizeof(int));
-    // right_c[i] = malloc(n*sizeof(int));
+    right_c[i] = malloc(n*sizeof(int));
   }
-  // left_memoized_LCS(m,n);
-  right_memoized_LCS(right_c, x_size,y_size);
-  // llcs = left_lookup_LCS(wordX, m, wordY, n);
-  rlcs = right_lookup_LCS(right_c, wordX, x_size, wordY, y_size);
-  printf("\nX : %s\n", wordX);
-  printf("Y : %s\n", wordY);
-  printf("RLCS : %d\n", rlcs);
-  show_matrix(right_c, m,n); 
-  // printf("\nLLCS encontrado : %d\n", llcs);
-  // show_matrix(left_c, m,n); 
-  // memoized_LCS(wordX, wordY);
 
-  free_matrix(left_c, m, n);
-  free_matrix(right_c, m,n);
+  initialize_right_LCS_matrix(right_c, m,n);
+  calculate_right_LCS_matrix(right_c, wordX, x_size, wordY, y_size);
+  calculate_L_vector(right_c, wordX, x_size, wordY, y_size, vectorL);
+  show_L_vector(vectorL,wordX, x_size, wordY);
+
+
+  show_matrix(right_c, m,n); 
+  free_matrix(right_c, m);
+}
+
+char* get_string(char* str){
+  size_t len = 0;
+  ssize_t read;
+
+  printf("Insira uma sequencia de caracteres : ");
+  if(getline(&str, &len, stdin) != -1){
+    size_t ln = strlen(str); 
+    if (ln > 0 && str[ln-1] == '\n') {str[--ln] = '\0';}
+    printf("Palavra lida : %s\n",str);
+    return str;
+  }else{
+    printf("Erro! Programa Abortado\n");
+    exit(EXIT_FAILURE);
+  }
 }
